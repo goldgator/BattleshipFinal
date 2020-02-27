e3be9df0fd32db4c5b7c85c37c9244c8f87ed59d
nicolesad <nsad308@gmail.com>
2020-02-27 11:06:20 -0700

    nicole init commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,8 @@ import com.company.view.GameView;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-       GameView view = new GameView();
+
+	       GameView view = new GameView();
     }
 }
 
